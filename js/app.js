67d1b5feb6f004adab04860e20cd754ce7a51b3b
JessieJ <jvwiltshire22@gmail.com>
2025-10-17 11:45:30 +0100

    MVP plus instructions

--- js/app.js
+++ js/app.js
@@ -1,35 +1,23 @@
-/*--------------------------------- Pseudo ---------------------------------*/
-// * Considerations:
 
-// * Inputs (user submitted or computer generated)
 
-// * Variables & State (think scores, choices, timers, lives etc)
+// global variables 
 
-// * User Interactions (user initiated events like clicks, hovers, key presses etc)
-
-// * Core Logic / Rules (List the core rules that will dictate the win lose condition)
-
-// * Conditions / Branching (which conditions will lead to which things happening)
-
-// * Loops (if any) (does any logic repeat? For example a ticking timer or in a game of poker maybe multiple computer choices generated on a loop)
-
-// * Outputs / Feedback (What will the app output to the screen)
-
-// Step 1 = creating grid
-
-let cellsEl; // creating this global variable to put cellsEl in the global scope to access later 
+let cellsEl; // creating this global variable to put cellsEl in the global scope to access later, essentially holding all the grid cells / array of DIVS 
 let width = 15 // this will be my number of columns
 let height = 15 // this will be my number of rows
+let gameInterval;
 
-// Step 8 - create food cell and make it appear in random locations 
+// creating grid
 
-const gridEl = document.querySelector('#grid'); // grabbing grid element from HTML via the DOM and storing for access 
+const gridEl = document.querySelector('#grid'); // grabbing grid element/container from HTML via the DOM and storing for access 
 
+// function to make the grid dynamically rather than hard code
 const makeGrid = () => {
 
     const cellCount = width * height; // cellCount now equates to 225 (15x15), not harcoding cellCount so I can change size of grid dynamically 
     // console.log(cellCount)
 
+    // forLoop to create each cell
     for (let i = 0; i < cellCount; i++) {
         const cell = document.createElement('div') // using document.createElement to create my a new div element and putting it in forLoop to x224 times
         // console.dir(cell);
@@ -49,21 +37,42 @@ makeGrid(); // calling the actual function tather than just declaring it
 // console.log(cellsEl[107])
 
 
-// Step 2: Creating Snake 
+// game over container & restart button 
+const gameOverBox = document.querySelector('#game-over');
+const restartBtn = document.querySelector('#restart-btn');
+
+  // Instructions Toggle 
+    const instructionsBtn = document.querySelector('#instructions-btn');
+    const instructionsBox = document.querySelector('#instructions-box');
+    const closeBtn = document.querySelector('#close-instructions');
+
+    instructionsBtn.addEventListener('click', () => {
+        console.log(instructionsBtn)
+        instructionsBox.classList.toggle('hidden');
+    });
+
+    closeBtn.addEventListener('click', () => {
+        instructionsBox.classList.add('hidden');
+    });
+
+
+
+// creating Snake 
 
 let snakeCellsLocation = [107, 108, 109, 110]; // storing the snake as and array of indices (numbers!) rather than DOM elements 
 // console.log(snakeCellsLocation)
 
-// Step 3: showing the snake on the board:
+// functionality to show the snake on the board:
 
 const showSnakeCells = () => {
     snakeCellsLocation.forEach((idx) => {
         cellsEl[idx].style.backgroundColor = 'green';
     })
 };
-showSnakeCells();
 
-// Step 4 - create event listeners for the 4 arrows
+showSnakeCells(); // calling the function to display snkae on board!
+
+// create event listeners for the 4 arrows
 
 let direction; // global variable to track the snake movement, using let as this is currently undefined and needs to change 
 
@@ -76,7 +85,7 @@ document.addEventListener('keydown', (event) => {
 });
 
 
-// Step 5: Moving the Snake in all directoins - add a cell to the array whilst removing a cell from the array on loop 
+// moving the Snake in all directions and checking for all possibilities
 
 const makeSnakeMove = () => {
     const snakeHead = snakeCellsLocation[snakeCellsLocation.length - 1]; // grabs the last element of the snake (the head) and stores it for later use
@@ -84,22 +93,21 @@ const makeSnakeMove = () => {
     let nextCellIndex; // will store where the snake is about to move but no value for this yet so null, defined below
     // console.log(nextCellIndex)
 
+    // determining the next cell based on direction of snkae 
     if (direction === 'right') nextCellIndex = headIndex + 1; // grid math of + 1 on the index, defining nextCellIndex based on travel direction
     else if (direction === 'left') nextCellIndex = headIndex - 1; //- 1 on the index
     else if (direction === 'up') nextCellIndex = headIndex - width; // go one row up so - 15 from cellindex
     else if (direction === 'down') nextCellIndex = headIndex + width; //go one row down so +15 from cell index
     else return;
 
-    // Determine next cell based on direction 
-
     const nextCell = cellsEl[nextCellIndex]; // stored cellsEl as an array (list) of grid cells, creating a variable (nextCell) as the DOM element and linking in to the next index
 
-// Step 6 edge detection 
+    // if snake moves but hits edge (edge detection )
 
-    if (nextCellIndex < 0) { 
+    if (nextCellIndex < 0) {
         // console.log('Ive hit the top!')
-        resetGame();
-        return;
+        resetGame(); // callnig my reset game function 
+        return; // return here is stopping the function early if this happens 
     } else if (nextCellIndex >= width * height) {
         // console.log('Ive hit the bottom!');
         resetGame();
@@ -114,23 +122,21 @@ const makeSnakeMove = () => {
         return;
     }
 
-// Step 7 checking for self-collision:
- 
+    // if snake moves but hits self (checking for self-collision)
+
     if (snakeCellsLocation.includes(nextCellIndex)) { //use .include method - Checks if any cell’s id matches the next index.
         console.log('Ive hit myself!');
-       resetGame();
+        resetGame();
         return;
     }
-    // highlight the new snake head 
 
-    else {
-        // safe to move on 
-        cellsEl[nextCellIndex].style.backgroundColor = 'green'; // keeps the head green
-        snakeCellsLocation.push(nextCellIndex);     // pushes the index to add a new head to the snake, not tpushing he element anymore (fix)
+    // If snake just moves and all is good (safe to move on)
+    cellsEl[nextCellIndex].style.backgroundColor = 'green'; // keeps the head green
+    snakeCellsLocation.push(nextCellIndex);     // pushes the index to add a new head to the snake, not tpushing he element anymore (fix)
 
-    }
 
-// Step 7, eating food and skipping removing the tail to allow snake to grow!!
+
+    // If snake moves and eats food (skip remvoing the tail to allow snake to grow!!)
 
     if (nextCellIndex === Number(foodCell.id)) { //reminder - Number(value) is a built-in JavaScript function...It takes anything that can represent a number(value) and converts it into a real number type.
         console.log('I ate the food!');
@@ -141,10 +147,11 @@ const makeSnakeMove = () => {
     }
 }
 
+// forLoop/whielLoop not ossible here as it would be too fast, method is setInterval to keep the loop moving at a given pace in ms
 gameInterval = setInterval(makeSnakeMove, 200);
 
 
-
+// functino to show food and move it in random places EXCEPT where the snake already is 
 const showFood = () => {
     // allocate food cell a random location
     let foodCellLocation = Math.floor(Math.random() * (height * width)); // math.floor ensures a whole number not decimal
@@ -157,36 +164,34 @@ const showFood = () => {
     // style the foor color red 
     foodCell.style.backgroundColor = "blue";
 }
+
 showFood();
+// function to reset game including clearing the interval / having a restart button / 
 
 const resetGame = () => {
     console.log("resetting game");
 
     clearInterval(gameInterval);
 
-    gridEl.innerHTML = ''; // clear old grid
-    makeGrid(); // rebuild grid
-    
+    gameOverBox.classList.remove('hidden');
 
-    snakeCellsLocation = [107, 108, 109, 110];
-    direction = undefined;
+    restartBtn.onclick = () => {
 
-    showSnakeCells();
-    showFood();
+        gameOverBox.classList.add('hidden');
 
-    gameInterval = setInterval(makeSnakeMove, 200);
+        gridEl.innerHTML = ''; // clear old grid
+        makeGrid(); // rebuild grid
 
-};
 
-// Instructions Toggle 
-const instructionsBtn = document.querySelector('#instructions-btn');
-const instructionsBox = document.querySelector('#instructions-box');
-const closeBtn = document.querySelector('#close-instructions');
+        snakeCellsLocation = [107, 108, 109, 110];
+        direction = undefined;
 
-instructionsBtn.addEventListener('click', () => {
-  instructionsBox.classList.toggle('hidden');
-});
+        showSnakeCells();
+        showFood();
 
-closeBtn.addEventListener('click', () => {
-  instructionsBox.classList.add('hidden');
-});
+        gameInterval = setInterval(makeSnakeMove, 200);
+
+    };
+
+  
+}
